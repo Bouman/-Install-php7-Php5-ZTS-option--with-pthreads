916784d5da71bd058cfa221e9fd3507c10f06fd3
Bouman <ndjbouman@gmail.com>
2021-03-30 21:38:23 +0200

    Update pthreads.sh

--- pthreads.sh
+++ pthreads.sh
@@ -12,7 +12,7 @@ mysql_secure_installation
 apt-get install build-essential apache2-dev libcurl4-openssl-dev libsqlite3-dev libmariadb-dev-compat:i386 libreadline-dev libzip-dev libxslt1-dev libicu-dev libmcrypt-dev libmhash-dev libpcre3-dev libjpeg-dev libfreetype6-dev libbz2-dev libxpm-dev bison re2c zlib1g-dev sqlite3 libsqlite3-dev libbz2-dev libcurl4-openssl-dev libenchant-dev libonig-dev libpspell-dev libedit-dev libreadline-dev libxslt-dev libwebp-dev libxpm-dev
 
 # Installation PHP7 (Without ZTS pthreads)
-apt-get install php7.2
+apt-get install php7.2 php-pear
 apt-get build-dep php7.2
 
 #Etre sur que curl est bien configuré
