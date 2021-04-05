450a3609c32581e182fc31ea3a1956c3bb46cb61
Bouman <ndjbouman@gmail.com>
2021-04-05 15:49:03 +0200

    Update pthreads-php5.sh

--- pthreads-php5.sh
+++ pthreads-php5.sh
@@ -1,15 +1,53 @@
-#Pré requis
-apt-get install gcc make libxml2-dev autoconf ca-certificates unzip nodejs curl libcurl4-openssl-dev pkg-config 
+#!/bin/bash
 
-#Dossier + Téléchargements
-mkdir /home/install
-cd /home/install
-wget http://be2.php.net/get/php-5.6.40.tar.bz2/from/this/mirror -O php-5.6.40.tar.bz2
-tar -xjvf php-5.6.40.tar.bz2
-cd php-5.6.40
+apt-get update
+apt-get install build-essential
 
-#Compilation et Installation
-./configure --disable-fileinfo --enable-maintainer-zts --enable-pthreads --prefix=/usr --with-config-file-path=/etc --with-curl --enable-cli --with-apxs2=/usr/bin/apxs2 \
+# Install Apache2
+apt-get install apache2
+
+# Install Mysql
+apt install mariadb-server
+mysql_secure_installation
+
+# Dependance Prérequis
+apt-get install build-essential composer libtool-bin apache2-dev libcurl4-openssl-dev libsqlite3-dev libreadline-dev libzip-dev libxslt1-dev libicu-dev libmcrypt-dev libmhash-dev libpcre3-dev libjpeg-dev libfreetype6-dev libbz2-dev libxpm-dev bison re2c zlib1g-dev sqlite3 libsqlite3-dev libbz2-dev libcurl4-openssl-dev libenchant-dev libonig-dev libpspell-dev libedit-dev libreadline-dev libxslt1-dev libwebp-dev libxpm-dev
+
+# Installation PHP7 (Without ZTS pthreads)
+apt-get install php5.6
+
+#Etre sur que curl est bien configuré
+# cd /usr/include
+# ln -s x86_64-linux-gnu/curl
+
+#icu-config configuration
+curl https://gist.githubusercontent.com/jasny/e91f4e2d386e91e6de5cf581795e9408/raw/16e2c42136eb3f214222c80d492e71942b77f174/icu-config > icu-config
+chmod +x icu-config
+mv icu-config /usr/bin
+
+# Deplacement à la base
+cd /
+
+#Telechargement PHP 5.6.40 + extraction et suppresion.
+wget http://cl1.php.net/get/php-5.6.40.tar.gz/from/this/mirror -O php-5.6.40.tar.gz
+tar zxvf php-5.6.40.tar.gz
+rm -rf ext/pthreads/
+rm php-5.6.40.tar.gz
+
+#Telechargement pthreads + movement dossier
+cd php-5.6.40/ext
+git clone https://github.com/krakjoe/pthreads -b master pthreads
+cd ..
+
+#Suppression des fichier PHP actuel
+rm -rf aclocal.m4
+rm -rf autom4te.cache/
+
+#Preparation + compilation
+./buildconf --force
+make distclean
+
+./configure --disable-fileinfo --enable-maintainer-zts --prefix=/usr --with-config-file-path=/etc --with-curl --enable-cli --with-apxs2=/usr/bin/apxs \
 --enable-mbstring \
     --enable-bcmath \
     --enable-calendar \
@@ -78,9 +116,57 @@ cd php-5.6.40
     --with-layout=GNU \
     --disable-rpath
 
-make
-make install   
-    
-#Téléchargement Ptreads
+make clear 
+make -j 4
+make install
+libtool --finish /php-5.6.40/libs
 
-    
+chmod o+x /usr/bin/phpize
+chmod o+x /usr/bin/php-config
+
+cd ext/pthreads*
+/usr/bin/phpize
+
+./configure --prefix=/usr --enable-pthreads=shared --with-php-config=/usr/bin/php-config
+make -j 4 && make install
+cd ../../
+
+#####Config PHP.INI DU SERVEUR#########
+#    nano php.ini-development         #
+# display_errors = Off ===> on        #
+# log_errors = On                     #
+# file_uploads = On                   #
+# upload_max_filesize = 2M  ====> 30M #
+# post_max_size = 8M  ====> 150M      #
+# max_execution_time = 30 ======> 120 #
+# short_open_tag ====> On             #
+# date.timezone = 'Europe/Paris'      #
+# memory_limit ====> 256M             #
+#                                     #
+#######################################
+
+cp php.ini-development /etc/php.ini
+cp php.ini-development /etc/php-cli.ini
+
+cp /etc/apache2/mods-available/php7.load /etc/apache2/mods-enabled/php7.load
+echo "<FilesMatch \.php$>
+    SetHandler application/x-httpd-php
+</FilesMatch>
+" >> /etc/apache2/mods-enabled/php7.conf
+
+#Suppression
+cd ..
+rm -rf php-5.6.40
+
+#extension on php.ini
+echo "extension=pthreads.so" >> /etc/php-cli.ini
+echo "zend_extension=opcache.so" >> /etc/php.ini
+
+#config
+export USE_ZEND_ALLOC=0
+
+# Time Zone Php.ini
+sed -i "s/^;date.timezone =$/date.timezone = \"Europe\/Paris\"/" /etc/php.ini |grep "^timezone" /etc/php.ini
+
+#Restart apache
+systemctl status apache2.service
