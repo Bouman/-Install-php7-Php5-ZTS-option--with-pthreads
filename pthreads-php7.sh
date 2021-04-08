ccd15e19fc175f3da72961df1c238e509abc4415
Bouman <ndjbouman@gmail.com>
2021-04-08 21:25:48 +0200

    Update pthreads-php7.sh

--- pthreads-php7.sh
+++ pthreads-php7.sh
@@ -13,46 +13,44 @@ apt install mariadb-server
 mysql_secure_installation
 
 # Dependance Prérequis
-apt-get install build-essential composer libtool-bin apache2-dev libcurl4-openssl-dev libsqlite3-dev libreadline-dev libzip-dev libxslt1-dev libicu-dev libmcrypt-dev libmhash-dev libpcre3-dev libjpeg-dev libfreetype6-dev libbz2-dev libxpm-dev bison re2c zlib1g-dev sqlite3 libsqlite3-dev libbz2-dev libcurl4-openssl-dev libenchant-dev libonig-dev libpspell-dev libedit-dev libreadline-dev libxslt1-dev libwebp-dev libxpm-dev
+apt-get install gcc make libxml2-dev autoconf ca-certificates unzip nodejs curl libcurl4-openssl-dev pkg-config
 
-# Installation PHP7 (Without ZTS pthreads)
-apt-get install php7.3
+#Ajout du dépôt 
+apt install ca-certificates apt-transport-https lsb-release
+wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add -
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+
+# Installation PHP5.6 (Without ZTS pthreads)
+apt update
+apt install php5.6
+
+#OPENSSL INSTALL v1.0.21 pour compil FOR PHP5.6 dans le dossier build-openssl
+apt-get install make 
+curl https://www.openssl.org/source/openssl-1.0.2l.tar.gz | tar xz && cd openssl-1.0.2l && ./config --prefix=/home/user/build-openssl && make -j 4 && make -j 4 install 
 
 #Etre sur que curl est bien configuré
-# cd /usr/include
-# ln -s x86_64-linux-gnu/curl
+cd /usr/include
+ln -s x86_64-linux-gnu/curl
 
 #icu-config configuration
 curl https://gist.githubusercontent.com/jasny/e91f4e2d386e91e6de5cf581795e9408/raw/16e2c42136eb3f214222c80d492e71942b77f174/icu-config > icu-config
 chmod +x icu-config
 mv icu-config /usr/bin
 
-# Deplacement à la base
-cd /
-
-#Telechargement PHP 7.3.27 + extraction et suppresion.
-wget http://cl1.php.net/get/php-7.3.27.tar.gz/from/this/mirror -O php-7.3.27.tar.gz
-tar zxvf php-7.3.27.tar.gz
-rm -rf ext/pthreads/
-rm php-7.3.27.tar.gz
-
-#Telechargement pthreads + movement dossier
-cd php-7.3.27/ext
-git clone https://github.com/krakjoe/pthreads -b master pthreads
-cd ..
-
-#PEAR install
-wget http://pear.php.net/go-pear.phar && php go-pear.phar
-
 #Suppression des fichier PHP actuel
 rm -rf aclocal.m4
 rm -rf autom4te.cache/
 
-#Preparation + compilation
-./buildconf --force
-make distclean
+# Deplacement à la base
+cd /
 
-./configure --disable-fileinfo --enable-maintainer-zts --prefix=/usr --with-config-file-path=/etc --with-curl --enable-cli --with-apxs2=/usr/bin/apxs \
+mkdir /home/install
+cd /home/install
+wget http://be2.php.net/get/php-5.6.40.tar.bz2/from/this/mirror -O php-5.6.40.tar.bz2
+tar -xjvf php-5.6.40.tar.bz2
+cd php-5.6.40
+
+./configure --disable-fileinfo --enable-maintainer-zts --prefix=/usr/local --with-config-file-path=/etc --with-curl --enable-cli --with-apxs2=/usr/bin/apxs \
 --enable-mbstring \
     --enable-bcmath \
     --enable-calendar \
@@ -86,7 +84,6 @@ make distclean
     --disable-cgi \
     --with-jpeg-dir=/usr/include/ \
     --with-xpm-dir=/usr/include/ \
-    --with-webp-dir=/usr/include/ \
     --with-png-dir=/usr/include/ \
     --with-enchant \
     --with-icu-dir=/usr \
@@ -103,7 +100,7 @@ make distclean
     --with-libzip \
     --with-pcre-regex \
     --with-openssl \
-    --with-openssl-dir=/usr/bin \
+    --with-openssl-dir=/home/user/build-openssl \
     --with-mysql-sock=/var/run/mysqld/mysqld.sock \
     --with-mysqli=mysqlnd \
     --with-sqlite3=/usr \
@@ -113,6 +110,7 @@ make distclean
     --with-readline \
     --with-tsrm-pthreads \
     --with-xsl \
+    --with-zlib \
     --with-zlib-dir=/usr \
     --with-fpm-user=www-data \
     --with-fpm-group=www-data \
@@ -121,57 +119,21 @@ make distclean
     --with-layout=GNU \
     --disable-rpath
 
-make clear 
-make -j 4
+make
 make install
-libtool --finish /php-7.3.27/libs
+cd /home/install
+wget http://pecl.php.net/get/pthreads-2.0.10.tgz
+tar -xvzf pthreads-2.0.10.tgz
+cd pthreads-2.0.10
+/usr/local/bin/phpize
+./configure
+make
+make install
+echo 'date.timezone = Europe/Paris' >> /usr/local/lib/php.ini
+echo 'extension=pthreads.so' >> /usr/local/lib/php.ini
 
-chmod o+x /usr/bin/phpize
-chmod o+x /usr/bin/php-config
-
-cd ext/pthreads*
-/usr/bin/phpize
-
-./configure --prefix=/usr --enable-pthreads=shared --with-php-config=/usr/bin/php-config
-make && make install
-cd ../../
-
-#####Config PHP.INI DU SERVEUR#########
-#    nano php.ini-development         #
-# display_errors = Off ===> on        #
-# log_errors = On                     #
-# file_uploads = On                   #
-# upload_max_filesize = 2M  ====> 30M #
-# post_max_size = 8M  ====> 150M      #
-# max_execution_time = 30 ======> 120 #
-# short_open_tag ====> On             #
-# date.timezone = 'Europe/Paris'      #
-# memory_limit ====> 256M             #
-#                                     #
-#######################################
-
-cp php.ini-development /etc/php.ini
-cp php.ini-development /etc/php-cli.ini
-
-cp /etc/apache2/mods-available/php7.load /etc/apache2/mods-enabled/php7.load
-echo "<FilesMatch \.php$>
-    SetHandler application/x-httpd-php
-</FilesMatch>
-" >> /etc/apache2/mods-enabled/php7.conf
-
-#Suppression
-cd ..
-rm -rf php-7.3.27
-
-#extension on php.ini
-echo "extension=pthreads.so" >> /etc/php-cli.ini
-echo "zend_extension=opcache.so" >> /etc/php.ini
-
-#config
-export USE_ZEND_ALLOC=0
-
-# Time Zone Php.ini
-sed -i "s/^;date.timezone =$/date.timezone = \"Europe\/Paris\"/" /etc/php.ini |grep "^timezone" /etc/php.ini
+#Restart apache
+systemctl status apache2.service
 
 #Restart apache
 systemctl status apache2.service
